1e702ef27f14533908248f216ce05ddc6ebfa9d8
zarembapawel <zarembapawel95@gmail.com>
2025-07-12 17:48:41 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # news
+
+## TODO
+- timer to get news 
+- save news to db
+- getting news from db 
+- searching news 
+- cache 
---
+++ src/test/java/pl/zarembapawel/news/NewsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zarembapawel.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
